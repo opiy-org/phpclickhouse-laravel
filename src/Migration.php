7c42274c19513150d4d45e5df95f714cf66bb184
alex14v <alexv14vvv@gmail.com>
2022-03-22 11:26:11 +0700

    fix migration

--- src/Migration.php
+++ src/Migration.php
@@ -4,21 +4,23 @@ declare(strict_types=1);
 
 namespace PhpClickHouseLaravel;
 
+use ClickHouseDB\Client;
+use ClickHouseDB\Statement;
 use Illuminate\Database\Migrations\Migration as BaseMigration;
 use Illuminate\Support\Facades\DB;
-use Tinderbox\Clickhouse\Client;
 
 class Migration extends BaseMigration
 {
     /**
      * @param string $sql
-     * @return bool
+     * @param array $bindings
+     * @return Statement
      */
-    protected static function write(string $sql): bool
+    protected static function write(string $sql, array $bindings = []): Statement
     {
         /** @var Client $client */
         $client = DB::connection('clickhouse')->getClient();
 
-        return $client->writeOne($sql);
+        return $client->write($sql, $bindings);
     }
 }
